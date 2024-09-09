37dcdd022f8d046ffc9b12b32e287e88a86f7890
Ricardo Lozano <ricardo.lozano.quesada@gmail.com>
2024-09-09 23:23:09 +0200

    errnoexample fixed

--- include/sample2_errno/errnoexample.c
+++ include/sample2_errno/errnoexample.c
@@ -2,25 +2,26 @@
 #include <stdio.h>
 #include <string.h>
   
-int main(int argc, char * argv[])
-{ 
+int main(int argc, char * argv[]) { 
     int returncode = 0;
     if (argc<2)
         returncode = -1;
     else {
         FILE* fp; 
         char * filename = argv[1];
-        fp = fopen(filename, "r");     
-        if (errno!=0){
-            printf("Value of errno is %d when openning %s\n", errno, filename); 
-            perror("Message from perror"); //prints on standard error
-            printf("The error message opening %s is : %s\n", filename, strerror(errno));
-            if (fp == NULL) printf("File descriptor value is NULL\n");
+        fp = fopen(filename, "r");
+        if (fp == NULL) {
+           printf("File descriptor value is NULL\n");     
+            if (errno!=0){
+                printf("Value of errno is %d when opening %s\n", errno, filename); 
+                perror("Message from perror"); //prints on standard error
+                printf("The error message opening %s is: %s\n", filename, strerror(errno));
+            }
+            returncode = -1;
         } else {
-            printf("No errors detected when openning %s\n",filename);
+            printf("No errors detected when opening %s\n",filename);
             fclose(fp);
         }
     }
-
     return returncode;
 }
